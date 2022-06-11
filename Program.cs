9ee758ca641bc81d7959b350f0cf621039e7e4ab
Alexander <aa.pigin@gmail.com>
2022-06-11 21:13:57 +0300

    Решение  третьей задачи

--- Program.cs
+++ Program.cs
@@ -100,3 +100,14 @@
 // int max = Math.Max(a, Math.Max(b, c));
 
 // Console.WriteLine($"Максимальное значение {max}");
+
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Введите число : ");
+// int a = Convert.ToInt32(Console.Read());
+// if (a % 2 == 0) Console.WriteLine("Четное");
+// else Console.WriteLine("Нечетное");
+
+
