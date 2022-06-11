1694b6dc2bc043973ddeb8095205181330f4d7f8
Alexander <aa.pigin@gmail.com>
2022-06-11 21:59:42 +0300

    Решение четвертой задачи

--- Program.cs
+++ Program.cs
@@ -111,3 +111,11 @@
 // else Console.WriteLine("Нечетное");
 
 
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    
+for (int i = 1; i <= n; i++)
+if (i % 2 == 0) Console.Write(i + " ");
