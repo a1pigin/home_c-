722a1552f372d369a2b9054dec712134107391df
Alexander <aa.pigin@gmail.com>
2022-06-11 20:36:42 +0300

    Решение первой задачи

--- Program.cs
+++ Program.cs
@@ -65,3 +65,20 @@
 // Console.Write("input N:");
 // int num = Convert.ToInt32(Console.ReadLine());
 // Console.Write(num % 10);
+
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите num1:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите num2:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2){
+    Console.WriteLine($"{num1} больше");
+    Console.WriteLine($"{num2} меньше");
+}
+
+else{
+     Console.WriteLine($"{num2} больше");
+    Console.WriteLine($"{num1} меньше");
+}
